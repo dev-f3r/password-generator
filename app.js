dcd37603e40dd498875444abe9f138d8e26dbcf4
dev-f3r <devfercastro@gmail.com>
2023-01-03 18:13:53 -0300

    script change

--- app.js
+++ app.js
@@ -13,10 +13,19 @@ const char = n => {
   return String.fromCharCode(n)
 }
 
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= 5; i++) {
+	if(i < 5){
   password += char(randomNum(65, 90)) // upp
   password += char(randomNum(97, 122))// lower
+	password += randomNum(0, 9) // num
   password += symbols[randomNum(0, symbols.length - 1)]// symb
+}else {
+	password += char(randomNum(65, 90)) // upp
+	password += symbols[randomNum(0, symbols.length - 1)] // symb
+}
 }
 
+// password += 'ñ'
+
 console.log(password)
+console.log(password.length)
